38ea98221153d70d25681c07c81fd2d8434b5b2b
daniel <dani.araujo2002@gmail.com>
2025-05-31 07:38:59 -0300

    add reaction repository impl

--- src/persistence/repositories/reaction.repository.impl.ts
+++ src/persistence/repositories/reaction.repository.impl.ts
@@ -2,44 +2,50 @@ import { Injectable } from "@nestjs/common";
 import { Reaction } from "src/domain/entities/reaction";
 import { ReactionRepository } from "src/domain/repositories/reaction.repository";
 import { PrismaService } from "src/persistence/config/prisma-service";
+import { ReactionMapper } from "../mappers/reaction.mapper";
 
 @Injectable()
 export class PrismaReactionRepository implements ReactionRepository {
     constructor(private readonly prisma: PrismaService) {}
 
     async create(reaction: Reaction): Promise<Reaction> {
-         await this.prisma.reaction.create({
-            data: reaction,
+        return await this.prisma.reaction.create({
+            data: ReactionMapper.toPrisma(reaction),
         });
-        return reaction;
     }
 
     async findById(id: string): Promise<Reaction | null> {
-        // implementation here
-        return null;
+        return await this.prisma.reaction.findUnique({
+            where: { id },
+        });
     }
 
     async findAll(): Promise<Reaction[]> {
-        // implementation here
-        return [];
+        return await this.prisma.reaction.findMany();
     }
 
     async update(id: string, reaction: Reaction): Promise<Reaction> {
-        // implementation here
-        return {} as Reaction;
+        return await this.prisma.reaction.update({
+            where: { id },
+            data: ReactionMapper.toPrisma(reaction),
+        });
     }
 
     async delete(id: string): Promise<void> {
-        // implementation here
+        await this.prisma.reaction.delete({
+            where: { id },
+        });
     }
 
-    async findByFeedbackId(feedbackId: string): Promise<Reaction[]> {
-        // implementation here
-        return [];
+    async findByFeedbackId(feedbackId: string): Promise<Reaction[]> { 
+        return await this.prisma.reaction.findMany({
+            where: { feedbackId },
+        });
     }
 
     async findByUserId(userId: string): Promise<Reaction[]> {
-        // implementation here
-        return [];
+        return await this.prisma.reaction.findMany({
+            where: { userId },
+        });
     }
 }
