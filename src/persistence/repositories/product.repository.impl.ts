3314f541a3e17b56685fb27cd3c6de933214d309
daniel <dani.araujo2002@gmail.com>
2025-05-31 05:59:56 -0300

    add product repository impl

--- src/persistence/repositories/product.repository.impl.ts
+++ src/persistence/repositories/product.repository.impl.ts
@@ -1,1 +1,46 @@
 import { Injectable } from "@nestjs/common";
+import { Product } from "src/domain/entities/product";
+import { ProductRepository } from "src/domain/repositories/product.repository";
+import { PrismaService } from "src/persistence/config/prisma-service";
+
+@Injectable()
+export class PrismaProductRepository implements ProductRepository {
+
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(product: Product): Promise<Product> {
+        return await this.prisma.product.create({
+            data: product,
+        });
+    }
+
+    async findById(id: string): Promise<Product | null> {
+        return await this.prisma.product.findUnique({
+            where: { id },
+        });
+    }
+
+    async findAll(): Promise<Product[]> {
+        return await this.prisma.product.findMany();
+    }
+
+    async update(id: string, product: Product): Promise<Product> {
+        return await this.prisma.product.update({
+            where: { id },
+            data: product,
+        });
+    }
+
+    async delete(id: string): Promise<void> {
+        await this.prisma.product.delete({
+            where: { id },
+        });
+    }
+
+    async findByUserId(userId: string): Promise<Product[]> {
+        return await this.prisma.product.findMany({
+            where: { userId },
+        });
+    }
+    
+}
