aa16c0722f13a4852c1d02d627758a95f0257133
daniel <dani.araujo2002@gmail.com>
2025-05-31 05:15:07 -0300

    add product usecase and product repository impl

--- src/domain/repositories/product.repository.ts
+++ src/domain/repositories/product.repository.ts
@@ -1,12 +1,10 @@
-import { Product } from '../product';
-import { Injectable } from '@nestjs/common';
+import { Product } from "../entities/product";
 
-@Injectable()
 export abstract class ProductRepository {
     abstract create(product: Product): Promise<Product>;
     abstract findById(id: string): Promise<Product | null>;
     abstract findAll(): Promise<Product[]>;
-    abstract update(id: string, product: Partial<Product>): Promise<Product>;
+    abstract update(id: string, product: Product): Promise<Product>;
     abstract delete(id: string): Promise<void>;
     abstract findByUserId(userId: string): Promise<Product[]>;
-}
+} 
