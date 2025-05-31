9fe70aca7a99de945301d050d5ed99da95498ee7
Bernardo Kirsch <bekirsch123@gmail.com>
2025-05-31 10:38:37 -0300

    aaa

--- src/persistence/controllers/feedback.controller.ts
+++ src/persistence/controllers/feedback.controller.ts
@@ -9,11 +9,84 @@ import {
   Post,
 } from '@nestjs/common'
 import { FeedbackUseCase } from 'src/aplication/usecases/feedbackUseCase'
+import { createFeedbackDto } from 'src/aplication/dtos/feedback/createFeedbackDto'
+import { FeedbackDto } from 'src/aplication/dtos/feedback/feedbackDto'
 
 
 @Controller('feedbacks')
 export class FeedbackController {
     constructor(private readonly feedbackUseCase: FeedbackUseCase) {}
 
+  @Post()
+  async createFeedback(@Body() feedbackDto: createFeedbackDto) {
+    try {
+      const feedback = await this.feedbackUseCase.createFeedback(feedbackDto)
+      return { statusCode: 200, data: feedback }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
 
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    try {
+      const feedback = await this.feedbackUseCase.findById(id)
+      if (!feedback) {
+        return { statusCode: 404, message: 'Feedback not found' }
+      }
+      return { statusCode: 200, data: feedback }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Get('/all')
+  async findAll() {
+    try {
+      const feedbacks = await this.feedbackUseCase.findAll()
+      return { statusCode: 200, data: feedbacks }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Patch()
+  async updateFeedback(@Body() feedbackDto: FeedbackDto) {
+    try {
+      const feedback = await this.feedbackUseCase.update(feedbackDto)
+      return { statusCode: 200, data: feedback }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Delete(':id')
+  async deleteFeedback(@Param('id') id: string) {
+    try {
+      await this.feedbackUseCase.delete(id)
+      return { statusCode: 200, message: 'Feedback deleted successfully' }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Get('/group/:groupId')
+  async findByGroupId(@Param('groupId') groupId: string) {
+    try {
+      const feedbacks = await this.feedbackUseCase.findByGroupId(groupId)
+      return { statusCode: 200, data: feedbacks }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Get('/author/:authorId')
+  async findByAuthorId(@Param('authorId') authorId: string) {
+    try {
+      const feedbacks = await this.feedbackUseCase.findByAuthorId(authorId)
+      return { statusCode: 200, data: feedbacks }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
 }
--- src/persistence/controllers/groups.controller.ts
+++ src/persistence/controllers/groups.controller.ts
@@ -9,11 +9,77 @@ import {
   Post,
 } from '@nestjs/common'
 import { GroupUseCase } from 'src/aplication/usecases/groupUseCase'
+import { createGroupDTO } from 'src/aplication/dtos/group/createGroupDTO'
+import { GroupDTO } from 'src/aplication/dtos/group/groupDTO'
 
 
 @Controller('groups')
 export class GroupController {
     constructor(private readonly groupUseCase: GroupUseCase) {}
+
+  @Post()
+  async createGroup(@Body() createGroupDTO: createGroupDTO) {
+    try {
+      const group = await this.groupUseCase.createGroup(createGroupDTO)
+      return { statusCode: 200, data: group }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    try {
+      const group = await this.groupUseCase.findById(id)
+      if (!group) {
+        return { statusCode: 404, message: 'Group not found' }
+      }
+      return { statusCode: 200, data: group }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Get('/all')
+  async findAll() {
+    try {
+      const groups = await this.groupUseCase.findAll()
+      return { statusCode: 200, data: groups }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Get('/parent/:parentId')
+  async findAllByParentId(@Param('parentId') parentId: string) {
+    try {
+      const groups = await this.groupUseCase.findAllByParentId(parentId)
+      return { statusCode: 200, data: groups }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Patch()
+  async updateGroup(@Body() groupDTO: GroupDTO) {
+    try {
+      const group = await this.groupUseCase.update(groupDTO)
+      return { statusCode: 200, data: group }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+
+  @Delete(':id')
+  async deleteGroup(@Param('id') id: string) {
+    try {
+      await this.groupUseCase.delete(id)
+      return { statusCode: 200, message: 'Group deleted successfully' }
+    } catch (error) {
+      return { statusCode: 500, message: error.message }
+    }
+  }
+  
 
 
 }
